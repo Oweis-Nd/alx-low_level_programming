c0695e7ec26f8901353257be507e2dbd42be5139
Oweis-Nd <oweilayefandoni@gmail.com>
2023-02-17 15:45:13 -0800

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -27,7 +27,9 @@ int main(void)
 	if (x < 6 && x != 0)
         printf("and is less than 6 and not 0");
         }
-        printf('\n');
- return (0);
+        else
+	printf("\n");
+{
+        return (0);
 }
 
