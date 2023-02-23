3ec19f45e85dfe5370245400f5f5b4e649321db8
Oweis-Nd <oweilayefandoni@gmail.com>
2023-02-23 03:57:01 -0800

    Task 9,Hot fix

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,28 +7,24 @@ void times_table(void)
 {
 	int i, j, k;
 
-		for (i = 0; i < 10; i++)
+		for (i = 0; i <= 9; i++)
 	{
-		for (j = 0; j < 10; j++)
+		_putchar(48);
+
+		for (j = 1; j <= 9; j++)
 		{
-			k = j + i;
-			if (j == 0)
+			k = i * j;
+			_putchar(44);
+			_putchar(32);
+			if (k <= 9)
 			{
-				_putchar(k + '0');
+				_putchar(32);
+				_putchar(k + 48);
 			}
-			if (k < 10 && j != 0)
+			else
 			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(k + '0');
-			}
-			else if (k >= 10)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar((k / 10) + '0');
-				_putchar((k % 10) + '0');
+				_putchar((k / 10) + 48);
+				_putchar((k % 10) + 48);
 			}
 		}
 		_putchar('\n');
