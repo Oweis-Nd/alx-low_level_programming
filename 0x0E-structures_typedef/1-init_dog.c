1e4df1d81c07824dffab91e68c05402fb5a7e06d
Oweis-Nd <oweilayefandoni@gmail.com>
2023-03-21 19:59:11 -0700

    1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -3,7 +3,7 @@
 #include "dog.h"
 
 /**
- * init_dog -intializes a variable of type struct dog
+ * init_dog - initializes a variable of type struct dog
  * @d: pointer to struct dog to initialize
  * @name: name to initialize
  * @age: age to initialize
